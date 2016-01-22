c625be4342d5455fdc3f5cfaa42b4b565deab7f0
Jim Welch <jwelch@decarta.com>
2016-01-22 16:00:30 -0500

    more minor changes

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
